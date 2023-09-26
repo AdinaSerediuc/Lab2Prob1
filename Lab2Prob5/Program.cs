b370493184aadfe5c5ecb637d2cef0fb604dec0c
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-26 23:26:04 +0300

    Lab2Prob5

---
+++ Lab2Prob5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2Prob5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Se citeste de la tastatura un vector continant n numere intregi, n fiind si el citit la
+               randul sau citit de la tastatura.
+               Scrieti functii care vor returna
+              • Cel mai mare numar din vector
+              • Cel mai mic numar din vector
+              • Numerele divizibile cu 3
+              • Scrieti o functie care va inversa elementele vectorului, apelati-o si afisati-I
+              rezultatul
+            */
+            //Console.Write("Introduceți elementele vectorului");
+            // string input = Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
