69eac105d46d38c60f199903041e23d45df98809
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-26 19:00:10 +0300

    Add project files.

---
+++ Lab2Prob1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2Prob1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
