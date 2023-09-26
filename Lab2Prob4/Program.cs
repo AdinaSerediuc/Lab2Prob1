223123792be442d994307a2fc6f75f7e0160dac2
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-26 22:48:42 +0300

    Lab2Prob4 add

--- Lab2Prob4/Program.cs
+++ Lab2Prob4/Program.cs
@@ -8,6 +8,25 @@ namespace Lab2Prob4
     {
         static void Main(string[] args)
         {
+            /*
+             * • Ex 4: Scrieti un program care va afisa piramida numerelor
+                 1
+                 2 2
+                 3 3 3
+                 4 4 4 4
+                 …
+                 n n n n n ….. n
+            */
+            Console.Write("Introduceti  numarul: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i =i+ 2)
+            {
+                for (int j = 0; j < i / 2; j++)
+                    {
+                    Console.Write(i + " " );
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
