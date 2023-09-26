c0d98d07c11b26d447e6b6519ba7c46eb95c9868
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-26 20:35:59 +0300

    Lab2Prob1

--- Lab2Prob1/Program.cs
+++ Lab2Prob1/Program.cs
@@ -8,6 +8,34 @@ namespace Lab2Prob1
     {
         static void Main(string[] args)
         {
+            /*
+             * Ex 1
+               Scrieti o functie care citeşte de la tastatură un şir de n numere naturale şi
+               determină media aritmetică a celor pare, n fiind citit de la tastatra
+            */
+            int n = int.Parse(Console.ReadLine());
+            int[] SirDeNumere = new int[n];
+            double Suma = 0;
+            int numar = 0;
+            double MediaAritmetica = 0;
+
+            for (int i = 0; i < SirDeNumere.Length; i++)
+            {
+                if (i % 2 == 0)
+                {                   
+                    numar = numar + 1;
+                    Suma = Suma + i;
+                    Console.WriteLine("Numar par " + i);                 
+                }
+            }
+            Console.WriteLine(numar);
+            Console.WriteLine("Suma numerelor pare este:" + Suma);
+            MediaAritmetica = Suma / numar;
+            Console.WriteLine("Media aritmetica este:" + MediaAritmetica);
+
+
+
+
         }
     }
 }
