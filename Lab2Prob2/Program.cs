b27dd9174127b6cfa3d5127085840fa1cb6de7d5
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-26 21:06:33 +0300

    Lab2Prob2 in progress

--- Lab2Prob2/Program.cs
+++ Lab2Prob2/Program.cs
@@ -8,6 +8,26 @@ namespace Lab2Prob2
     {
         static void Main(string[] args)
         {
+            /*
+             * Ex 2
+          Scrieti o functie care va determina daca un numar este sau nu palindrom.
+          */
+            string cuvant = string.Parse(Console.ReadLine());
+            int lungime = cuvant.Length;
+
+            for (int i = 0; i < lungime % 2; i++)
+            {
+                if (cuvant[i] <= cuvant[lungime - 1 - i]) ;
+                {
+
+                    Console.WriteLine("Cuvantul nu este palindrom" + cuvant);
+                }
+                else
+                { Console.WriteLine("Cuvantul este palindrom:" + cuvant);
+                }
+            }
+
+            }
         }
     }
 }
